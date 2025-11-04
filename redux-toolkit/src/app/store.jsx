e1e5d55bc541d65743b9f20696657beebe9b5328
ImrenR <imrenrahbay@gmail.com>
2025-11-04 13:35:39 +0000

    store.jsx

--- redux-toolkit/src/app/store.jsx
+++ redux-toolkit/src/app/store.jsx
@@ -1,1 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+import yetkiSlice from "../features/yetkiSlice";
 
+
+
+
+export const store = configureStore({
+  reducer: {
+    permission : yetkiSlice,
+  },
+})
