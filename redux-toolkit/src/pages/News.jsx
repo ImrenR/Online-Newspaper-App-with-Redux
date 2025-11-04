b33e43ed7cd4dcf9a06a965d7466a4071cd677e8
ImrenR <imrenrahbay@gmail.com>
2025-11-04 16:56:58 +0000

    up

--- redux-toolkit/src/pages/News.jsx
+++ redux-toolkit/src/pages/News.jsx
@@ -1,4 +1,4 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import loadingGif from "../assets/loading.gif";
 
 import {
@@ -14,8 +14,9 @@ import {
 
 const News = () => {
   
+  
   const {haberler, loading}=useSelector((state)=> state.haberSlice)
-console.log(haberler)
+const dispatch= useDispatch()
   return (
     <>
     {loading ?  (<img src={loadingGif} alt="" height="800px" />)
@@ -27,6 +28,27 @@ console.log(haberler)
           justifyContent="space-evenly"
           flexWrap="wrap"
         >
+          {haberler.map((a)=>
+          <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        title="haber"
+        sx={{height : 140}}
+        image={a.urlToImage}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {a.title}
+        </Typography>
+        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+          {a.content}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small"
+        onClick={()=>dispatch(clear(a.url))}>Clear</Button>
+        <Button href={a.url} size="small">Detail</Button>
+      </CardActions>
+    </Card> )}
           
         </Box>
    ) }
