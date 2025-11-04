8158e4757e54afe9cc3201810abacb772c400dfd
ImrenR <imrenrahbay@gmail.com>
2025-11-04 14:47:52 +0000

    update

--- redux-toolkit/src/router/AppRouter.jsx
+++ redux-toolkit/src/router/AppRouter.jsx
@@ -3,14 +3,19 @@ import Navbar from '../components/Navbar'
 import { Route, Routes } from 'react-router-dom'
 import Login from '../pages/Login'
 import News from '../pages/News'
+import PrivateRouter from './PrivateRouter'
 
 const AppRouter = () => {
   return (
     <div>
       <Navbar/>
       <Routes>
-           <Route path='/' element={<News/>} />
+         
         <Route path='/login' element={<Login/>} />
+
+        <Route path='/' element={<PrivateRouter/>}>
+  <Route path='' element={<News/>} />
+        </Route>
       </Routes>
     </div>
   )
--- redux-toolkit/src/router/PrivateRouter.jsx
+++ redux-toolkit/src/router/PrivateRouter.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import { useSelector } from "react-redux"
+import { Navigate, Outlet } from "react-router-dom"
+
 
 const PrivateRouter = () => {
-  return (
-    <div>PrivateRouter</div>
-  )
+
+  const {email,password}=useSelector((state)=>state.yetkiSlice)
+  console.log(email)
+  return email === "imren" && password === "12345" ? (<Outlet/>) : <Navigate to="/login"/>
 }
 
 export default PrivateRouter
