452416d577c1a7ca79db13d581a6d0a1d3356eed
ImrenR <imrenrahbay@gmail.com>
2025-11-04 15:26:51 +0000

    update

--- redux-toolkit/src/features/haberSlice.jsx
+++ redux-toolkit/src/features/haberSlice.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { createSlice } from "@reduxjs/toolkit";
 
-const haberSlice = () => {
-  return (
-    <div>haberSlice</div>
-  )
-}
+const haberSlice = createSlice({
+  name: haberSlice,
+  initialState: {
+    haberler: [],
+    loading: true,
+  },
+  reducers: {
+    clear: () => {},
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(incrementAsync.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(incrementAsync.fullfilled, (state, action) => {
+        state.status = "idle";
+        state.value += action.payload;
+      });
+  },
+});
 
-export default haberSlice
+export default haberSlice;
