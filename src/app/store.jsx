78123dc9093b32b6839204fa68de2ffb72fe79ca
ImrenR <imrenrahbay@gmail.com>
2025-11-07 14:02:27 +0300

    update

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -1,8 +1,7 @@
+
 import { configureStore } from "@reduxjs/toolkit";
 import yetkiSlice from "../features/yetkiSlice";
 import haberSlice from "../features/haberSlice";
-
-
 
 
 export const store = configureStore({
